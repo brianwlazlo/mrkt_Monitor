7a7eb6e8da2f4449c600ece010d501432136fdb4
Brian Wlazlo <brianwlazlo7@gmail.com>
2020-12-08 22:23:01 -0500

    added function to get and display company name

--- index.js
+++ index.js
@@ -5,31 +5,19 @@
     //All sandbox endpoints function the same as production, so you will only need to change the base url and token
 
 
-//-----MOST USEFUL FOR ACTUALLY CONSTRUCTING PRACTICAL APP ------------//
-  // base URL for Paid Access = https://cloud.iexapis.com/v1/
-  // API tokens must be passed with each request
-  // need a "Publishable" API token
-  // HTTP REQUEST
-    //GET /stock/{ticker}/batch
-    // ex. /stock/aapl/batch?types=quote,news,chart&range=1m&last=10
-    // Query String parameters ==> "types" is REQUIRED, "symbols" is Optionable and only used with multiple tickers, "range" is Optional and only needed if chart is used in 'types' parameter
-    // 'filter' can be used to return a subset of data ex. "?filter=symbol,volume,lastSalePrice" will return only these 3 data points
-  // JSON is the default data format
-
-
-//------MOST USEFUL URL END POINTS FOR TESTING ----//
-  //Light wight Data points ... GET /data-points/{symbol}/{key} (where 'key' = Quote-LatestPrice, or Latest-Financial-Report-Date, Latest-News)
-    // data-points/aapl/QUOTE-LATESTPRICE
-    // OR COULD ALSO USE 'price only' ....GET /stock/{symbol}/price....will return a single number as price.
-  
-  // Historical Prices (to get OHLC+Volume)....GET /stock/{symbol}/chart/{range}/{date}  ex. /stock/twtr/chart/5d  OR (for what we need) /stock/twtr/chart/date/YYYMMDD 
-      // OR COULD ALSO USE....Previous Day Price....GET /stock/{symbol}/previous
-  
-  // NEWS INFORMATION: 
-    // GET.../stock/{symbol}/news/last/{#ofArticlesToReturn}. Elements to use are 'headline', 'source', 'url', 'summary', 'img'
 
 const baseURL = "https://sandbox.iexapis.com/stable/stock"
 const apiTesterToken = "Tpk_0d2324a0af6c4d1d87f32ea3445f31e8"
+
+// get company Name
+function getCompanyName (ticker) {
+  let companyInfo = `${baseURL}/${ticker}/company?token=${apiTesterToken}`
+  console.log(companyInfo);
+
+  fetch(companyInfo)
+    .then(companyInfo => companyInfo.json())
+    .then(companyInfoJson => displayCompanyName(companyInfoJson))
+}
 
 // get OHLC, volume, dailyRange
 function getTickerPriceInfo (ticker) {
@@ -50,16 +38,23 @@ function getTickerNews (ticker) {
   
 }
 
+//display Company Name
+function displayCompanyName(companyInfoJson) {
+  console.log(companyInfoJson.companyName);
+}
+
+
 //display OHLC, Vol, Range buttons
 function displayPriceData(priceJson) {
-  console.log("High: " + priceJson.high + " Low: " + priceJson.low)
+  //console.log("High: " + priceJson.high + " Low: " + priceJson.low)
 }
+
 
 //display news Headlines (with link, summary and pic)
 function displayNewsHeadlines(headlinesJson) {
 
   for (let i=0; i<headlinesJson.length; i++) {
-    console.log("Headline: " + headlinesJson[i].headline)
+    //console.log("Headline: " + headlinesJson[i].headline)
   };
 
 }
@@ -70,6 +65,7 @@ function watchForm () {
     let ticker = $('#ticker-symbol-search').val().toUpperCase();
     getTickerNews(ticker);
     getTickerPriceInfo(ticker);
+    getCompanyName(ticker);
 
   })
   
